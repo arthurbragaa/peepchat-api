f284243f1c89f6b2eaade7349369cf47aea15e61
Arthur Alfredo <arthurbalfredo@gmail.com>
2016-04-26 21:34:46 -0300

    Ready for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -4,7 +4,7 @@ config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [host: "ancient-brook-10081.herokuapp.com", port: 80],
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :peepchat, Peepchat.Repo,
   adapter: Ecto.Adapters.Postgres,
