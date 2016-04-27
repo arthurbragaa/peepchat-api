ffbcdef59c048aa4abac0b1daf22fd8916c1c48c
Arthur Alfredo <arthurbalfredo@gmail.com>
2016-04-26 21:33:53 -0300

    Ready for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -2,10 +2,9 @@ use Mix.Config
 
 config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "ancient-brook-10081.herokuapp.com", port: 80],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  cache_static_manifest: "priv/static/manifest.json"
-
 
 config :peepchat, Peepchat.Repo,
   adapter: Ecto.Adapters.Postgres,
